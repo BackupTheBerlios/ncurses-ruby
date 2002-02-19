b0563bd8cc1b154fa662d9f71962b43c375c5ad5
t-peters <t-peters>
2002-02-19 22:30:34 +0000

    #includes     * added group.hh and object.hh: Code now uses WrGroup and WrObject                                     static methods
      Tvision_Ruby::WrApplication::WrApplication
        * The new WrApplication object and its ruby wrapper is now registered as a
          class variable of the Ruby class Tvision::Application. Only the id of the
          ruby object is registered to allow Garbage Collection.
    
      Tvision_Ruby::WrApplication::rb_new
        * accesses the created Ruby object through the newly introduced class
          variable. Calls Tvision_Ruby::WrApplication::rb_application for this
          purpose. Registers the new Application object in the TObject database in
          class WrObject.
    
      Tvision_Ruby::WrApplication::c_initStatusLine
        * method removed
    
      Tvision_Ruby::WrApplication::rb_initStatusLine
        * uses Tvision_Ruby::WrApplication::unwrap
        * superseeds Tvision_Ruby::WrApplication::c_initStatusLine
    
      Tvision_Ruby::WrApplication::c_initMenuBar
        * method removed
    
      Tvision_Ruby::WrApplication::rb_initMenuBar
        * uses Tvision_Ruby::WrApplication::unwrap
        * superseeds Tvision_Ruby::WrApplication::c_initMenuBar
    
      Tvision_Ruby::WrApplication::c_initDeskTop
        * method removed
    
      Tvision_Ruby::WrApplication::rb_initDeskTop
        * uses Tvision_Ruby::WrApplication::unwrap
        * superseeds Tvision_Ruby::WrApplication::c_initDeskTop
    
      Tvision_Ruby::WrApplication::rb_mark
        * calls (ruby-)superclass rb_mark method
    
      Tvision_Ruby::WrApplication::rb_suspend
        * new method
        * provides Application#suspend
    
      Tvision_Ruby::WrApplication::rb_resume
        * new method
        * provides Application#resume
    
      Tvision_Ruby::WrApplication::init_wrapper
        * superclass for Application is Group (folding Program completely into
                                               Application)
        * set class variable Application::@@application_id
        * set class constant Application::SLEEPING_TIME to 0.1(seconds) in each
          event loop iteration.
        * register Tvision_Ruby::WrApplication::rb_suspend as Application#suspend
        * register Tvision_Ruby::WrApplication::rb_resume  as Application#resume
        * register Tvision_Ruby::WrApplication::rb_application as
          Application::application
        * initialize tvisions global variable TProgram::doNotReleaseCPU to 1
          (Tvision will not sleep in its own mainloop then)
    
      call_id2ref
        * new local function. Accesses the ruby object with a givem ID if it exists
    
      Tvision_Ruby::WrApplication::rb_application
        * new static method
        * implements read access to the single Application instance if one exists
    
      Tvision_Ruby::WrApplication::wrap
        * new static method
        * wraps C++ TApplication objects in Ruby application objects. For objects
          of this class, the wrapper has been created on C++ object creation.
    
      Tvision_Ruby::WrApplication::unwrap
        * new static method
        * provides access to the C++ TApplication object of the given Ruby
          Application object
    
      Tvision_Ruby::WrApplication::rb_idle
        * new static method
        * implements Application#idle
    
      Tvision_Ruby::WrApplication::idle
        * new virtual method
        * delegates call from framework to ruby Application object
    
      Tvision_Ruby::WrApplication::initScreen
        * new virtual method
        * delegates call from framework to ruby Application object
    
      Tvision_Ruby::WrApplication::rb_initScreen
        * new static method
        * implements Application#initScreen
    
      Tvision_Ruby::WrApplication::outOfMemory
        * new virtual method
        * delegates call from framework to ruby Application object
    
      Tvision_Ruby::WrApplication::rb_outOfMemory
        * new static method
        * implements Application#outOfMemory

--- tvision-ruby/application.cc
+++ tvision-ruby/application.cc
@@ -22,8 +22,11 @@
 // $ID$
 
 #include "application.hh"
+#include "group.hh"
+#include "object.hh"
 
 VALUE Tvision_Ruby::WrApplication::cTApplication = 0;
+VALUE Tvision_Ruby::WrApplication::sleep_time    = 0;
 
 Tvision_Ruby::WrApplication::WrApplication(VALUE c_application)
     // set createStatusLine, createMenuBar, and createDeskTop to 0. They
@@ -38,7 +41,10 @@ Tvision_Ruby::WrApplication::WrApplication(VALUE c_application)
                                   this);
 
     // register this application object in a global variable
-    rb_iv_set(Tvision_Ruby::mTvision, "@application", self);
+    rb_cv_set(Tvision_Ruby::WrApplication::cTApplication, "@@application_id",
+              rb_funcall(self, rb_intern("__id__"), 0));
+    // we only register it's id to enable Garbage Collection of an unused
+    // Application object
 
     // The ruby initializer might call the methods initStatusLine,
     // initMenuBar and initDeskTop.
@@ -64,8 +70,8 @@ Tvision_Ruby::WrApplication::rb_new(VALUE application_class)
     VALUE ancestors = rb_funcall(application_class, rb_intern("ancestors"), 0);
     if (RTEST(rb_funcall(ancestors, rb_intern("include?"), 1,
                          Tvision_Ruby::WrApplication::cTApplication))) {
-        VALUE          rb_app = rb_iv_get(Tvision_Ruby::mTvision,
-                                          "@application");
+        VALUE  rb_app = Tvision_Ruby::WrApplication::
+            rb_application(Tvision_Ruby::WrApplication::cTApplication);
         if (RTEST(rb_app)) {
             rb_raise(rb_eRuntimeError, "Only one Tvision application object "
                      "may be created in each ruby application.  There is "
@@ -73,9 +79,19 @@ Tvision_Ruby::WrApplication::rb_new(VALUE application_class)
                      STR2CSTR(rb_inspect(rb_app)));
             return Qnil;
         }
+        // create a new C++ WrApplication object. It will be registered in the
+        // @@application_id class variable of the ruby class
+        // Tvision::Application
         WrApplication * c_app = new WrApplication(application_class);
-        rb_app = rb_iv_get(Tvision_Ruby::mTvision, "@application");
-        if (DATA_PTR(rb_app) != c_app) {
+
+        // Ensure the Application object is registered in the map that lists
+        // the ruby object corresponding to a c++ object. This will call
+        // WrApplication::wrap now, which will call delegate the call to
+        // WrApplication::rb_application
+        rb_app = Tvision_Ruby::WrObject::wrap(*c_app);
+
+        // Ensure that we have only one Application Object
+        if (TYPE(rb_app) != T_DATA || DATA_PTR(rb_app) != c_app) {
             rb_raise(rb_eRuntimeError, "Bug in Tvision wrapper: could not "
                      "create application object.  Maybe two application "
                      "objects were created simultaneously in different "
@@ -95,57 +111,56 @@ Tvision_Ruby::WrApplication::rb_new(VALUE application_class)
 VALUE
 Tvision_Ruby::WrApplication::rb_initStatusLine(VALUE self)
 {
-    WrApplication * c_app;
-    Data_Get_Struct(self, WrApplication, c_app);
-    c_app->c_initStatusLine();
+    TApplication * c_app = &Tvision_Ruby::WrApplication::unwrap(self);
+    TApplication::statusLine =
+        TApplication::initStatusLine(c_app->getExtent());
+    c_app->insert(statusLine);
     return Qnil;
-}
-void
-Tvision_Ruby::WrApplication::c_initStatusLine(void)
-{
-    statusLine = TApplication::initStatusLine(getExtent());
-    insert(statusLine);
 }
 
 VALUE
 Tvision_Ruby::WrApplication::rb_initMenuBar(VALUE self)
 {
-    WrApplication * c_app;
-    Data_Get_Struct(self, WrApplication, c_app);
-    c_app->c_initMenuBar();
+    TApplication * c_app = &Tvision_Ruby::WrApplication::unwrap(self);
+    TApplication::menuBar = TApplication::initMenuBar(c_app->getExtent());
+    c_app->insert(menuBar);
     return Qnil;
-}
-void
-Tvision_Ruby::WrApplication::c_initMenuBar(void)
-{
-    menuBar = TApplication::initMenuBar(getExtent());
-    insert(menuBar);
 }
 
 VALUE
 Tvision_Ruby::WrApplication::rb_initDeskTop(VALUE self)
 {
-    WrApplication * c_app;
-    Data_Get_Struct(self, WrApplication, c_app);
-    c_app->c_initDeskTop();
+    TApplication * c_app = &Tvision_Ruby::WrApplication::unwrap(self);
+    TApplication::deskTop = TApplication::initDeskTop(c_app->getExtent());
+    c_app->insert(deskTop);
     return Qnil;
-}
-void
-Tvision_Ruby::WrApplication::c_initDeskTop(void)
-{
-    deskTop = TApplication::initDeskTop(getExtent());
-    insert(deskTop);
 }
 
 void
-Tvision_Ruby::WrApplication::rb_mark(void*)
+Tvision_Ruby::WrApplication::rb_mark(void * c_app)
 {
-    // Not yet anything to mark
+    Tvision_Ruby::WrGroup::rb_mark(c_app);
 }
 void
 Tvision_Ruby::WrApplication::rb_free(void * c_app)
 {
     TObject::destroy(reinterpret_cast<Tvision_Ruby::WrApplication*>(c_app));
+}
+
+
+VALUE
+Tvision_Ruby::WrApplication::rb_suspend(VALUE self)
+{
+    TApplication * c_app = &Tvision_Ruby::WrApplication::unwrap(self);
+    static_cast<WrApplication *>(c_app)->suspend();
+    return self;
+}
+VALUE
+Tvision_Ruby::WrApplication::rb_resume(VALUE self)
+{
+    TApplication * c_app = &Tvision_Ruby::WrApplication::unwrap(self);
+    static_cast<WrApplication *>(c_app)->resume();
+    return self;
 }
 
 void
@@ -154,8 +169,19 @@ Tvision_Ruby::WrApplication::init_wrapper(void)
     // define class Application in module Tvision
     Tvision_Ruby::WrApplication::cTApplication =
         rb_define_class_under(Tvision_Ruby::mTvision, "Application",
-                              rb_cObject);
+                              Tvision_Ruby::WrApplication::cTApplication);
 
+    // set a class variable in this class to the id of the currently living
+    // Application object
+    rb_define_class_variable(Tvision_Ruby::WrApplication::cTApplication,
+                             "@@application_id", Qnil);
+
+    // set a class constant in this class to the prefered sleeping time
+    rb_define_const(Tvision_Ruby::WrApplication::cTApplication,
+                    "SLEEPING_TIME",
+                    Tvision_Ruby::WrApplication::sleep_time =
+                    rb_float_new(0.1));
+    
     // overwrite Application::new(). It will allocate a C++ TApplication
     rb_define_module_function(Tvision_Ruby::WrApplication::cTApplication,
                               "new",
@@ -195,4 +221,133 @@ Tvision_Ruby::WrApplication::init_wrapper(void)
     rb_funcall(Tvision_Ruby::WrApplication::cTApplication,
                rb_intern("private"), 2,
                rb_str_new2("clone"), rb_str_new2("dup"));
+
+    // Application#suspend() temporarily exits the ncurses screen usage mode.
+    // Useful for shelling out.
+    rb_define_method(Tvision_Ruby::WrApplication::cTApplication,
+                     "suspend",
+                     reinterpret_cast<VALUE(*)(...)>
+                     (&Tvision_Ruby::WrApplication::rb_suspend), 0);
+    
+    // Application#resume() restores the TUI screen after a call to suspend()
+    rb_define_method(Tvision_Ruby::WrApplication::cTApplication,
+                     "resume",
+                     reinterpret_cast<VALUE(*)(...)>
+                     (&Tvision_Ruby::WrApplication::rb_resume), 0);
+    rb_define_module_function(Tvision_Ruby::WrApplication::cTApplication,
+                              "application",
+                              reinterpret_cast<VALUE(*)(...)>
+                              (&Tvision_Ruby::WrApplication::rb_application),
+                              0);
+
+    // we implement our own CPU release in WrApplication::idle
+    TProgram::doNotReleaseCPU = 1;
 };
+
+static VALUE call_id2ref(VALUE id) {
+    VALUE rb_mObjectSpace = rb_const_get(rb_cObject, rb_intern("ObjectSpace"));
+    VALUE app = rb_funcall(rb_mObjectSpace, rb_intern("_id2ref"), 1, id);
+    return app;
+}
+VALUE
+Tvision_Ruby::WrApplication::rb_application(VALUE)
+{
+    int exception_raised = 0;
+    VALUE id = rb_cv_get(Tvision_Ruby::WrApplication::cTApplication,
+                         "@@application_id");
+    if (id == Qnil)
+        return Qnil;
+    VALUE app = rb_protect(reinterpret_cast<VALUE(*)(...)>(&call_id2ref), id,
+                           &exception_raised);
+    if (exception_raised) {
+        rb_cv_set(Tvision_Ruby::WrApplication::cTApplication,
+                  "@@application_id", Qnil);
+        return Qnil;
+    }
+    return app;
+}
+VALUE
+Tvision_Ruby::WrApplication::wrap(TApplication & c_app)
+{
+    VALUE rb_app = Tvision_Ruby::WrApplication::
+        rb_application(Tvision_Ruby::WrApplication::cTApplication);
+    if (rb_app == Qnil) {
+        rb_raise(rb_eRuntimeError, "C++:Tvision_Ruby::WrApplication::wrap: "
+                 "cannot wrap TApplication object outside "
+                 "Ruby:Tvision::Application::new constructor. File: %s, "
+                 "Line: %d", __FILE__, __LINE__);
+        return Qnil;
+    }
+    if (&Tvision_Ruby::WrApplication::unwrap(rb_app) != &c_app) {
+        rb_raise(rb_eRuntimeError, "C++:Tvision_Ruby::WrApplication::wrap: "
+                 "cannot wrap additional TApplication object, and outside "
+                 "Ruby:Tvision::Application::new constructor. File: %s, "
+                 "Line: %d", __FILE__, __LINE__);
+        return Qnil;
+    }
+    return rb_app;
+}
+
+
+Tvision_Ruby::WrApplication &
+Tvision_Ruby::WrApplication::unwrap(VALUE rb_application)
+{
+    Tvision_Ruby::WrApplication * c_application =
+        dynamic_cast<Tvision_Ruby::WrApplication *>
+        (&Tvision_Ruby::WrGroup::unwrap(rb_application));
+    if (c_application) {
+        return *c_application;
+    }
+    rb_raise(rb_eRuntimeError, "C++:Tvision_Ruby::WrApplication::unwrap: "
+             "VALUE %x does not wrap a WrApplication. File: %s, Line: %d",
+             rb_application, __FILE__, __LINE__);
+    return *c_application;
+}
+
+VALUE
+Tvision_Ruby::WrApplication::rb_idle(VALUE rb_application)
+{
+    TProgram::doNotReleaseCPU = 1;
+    Tvision_Ruby::WrApplication::unwrap(rb_application).TApplication::idle();
+
+    // sleep inside the ruby interpreter to allow internal multithreading
+    rb_funcall(rb_mKernel, rb_intern("sleep"), 1,
+               Tvision_Ruby::WrApplication::sleep_time);
+    return Qnil;
+}
+void
+Tvision_Ruby::WrApplication::idle()
+{
+    // delegate virtual function call to the corresponding ruby method
+    rb_funcall(Tvision_Ruby::WrApplication::wrap(*this), rb_intern("idle"), 0);
+}
+
+
+void
+Tvision_Ruby::WrApplication::initScreen()
+{
+    // delegate virtual function call to the corresponding ruby method
+    rb_funcall(Tvision_Ruby::WrApplication::wrap(*this),
+               rb_intern("initScreen"), 0);
+}
+VALUE
+Tvision_Ruby::WrApplication::rb_initScreen(VALUE rb_application)
+{
+    Tvision_Ruby::WrApplication::
+        unwrap(rb_application).TApplication::initScreen();
+    return Qnil;
+}
+
+void
+Tvision_Ruby::WrApplication::outOfMemory()
+{
+    // delegate virtual function call to the corresponding ruby method
+    rb_funcall(Tvision_Ruby::WrApplication::wrap(*this),
+               rb_intern("outOfMemory"), 0);
+}
+VALUE
+Tvision_Ruby::WrApplication::rb_outOfMemory(VALUE rb_application)
+{
+    Tvision_Ruby::WrApplication::unwrap(rb_application).outOfMemory();
+    return Qnil;
+}
