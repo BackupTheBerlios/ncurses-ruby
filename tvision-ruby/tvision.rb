8639a28d286ef4f1a21d460c602739120de44dcb
t-peters <t-peters>
2002-02-28 15:44:44 +0000

    included copyright statement and e-mail-adress   renamed Tvision.destroy to TVision.destroy

--- tvision-ruby/tvision.rb
+++ tvision-ruby/tvision.rb
@@ -1,4 +1,22 @@
-# $Id: tvision.rb,v 1.5 2002/02/27 10:30:34 t-peters Exp $
+# $Id: tvision.rb,v 1.6 2002/02/28 15:44:44 t-peters Exp $
+
+# tvision-ruby is a ruby wrapper for the text-mode application framework
+# developed by Borland.
+# Copyright (C) 2002 Tobias Peters <t-peters@users.berlios.de>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 require "tvision.so"
 
@@ -293,7 +311,7 @@ module TVision
     end
   end
 
-  def Tvision.destroy(object)
+  def TVision.destroy(object)
     object.shutDown
   end
 
