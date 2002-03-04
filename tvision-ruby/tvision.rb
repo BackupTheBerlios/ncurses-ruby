c614fc2487ede7dab0e86e3bca06e386b09dfab3
t-peters <t-peters>
2002-03-04 07:11:13 +0000

    constants      * Added keyboard constants to module TVision
       TVision::Rect
         * removed old, conflicting implementation in pure ruby
         * added method Rect#inspect

--- tvision-ruby/tvision.rb
+++ tvision-ruby/tvision.rb
@@ -1,4 +1,4 @@
-# $Id: tvision.rb,v 1.6 2002/02/28 15:44:44 t-peters Exp $
+# $Id: tvision.rb,v 1.7 2002/03/04 07:11:13 t-peters Exp $
 
 # tvision-ruby is a ruby wrapper for the text-mode application framework
 # developed by Borland.
@@ -229,6 +229,86 @@ module TVision
   focusedEvents       = EvKeyboard | EvCommand;
 
 
+  # Keys
+
+  KbSpace     = 0x0034;
+
+    #  Control keys
+
+    KbCtrlA=0x0101;KbCtrlB=0x0102;KbCtrlC=0x0103;KbCtrlD=0x0104;
+    KbCtrlE=0x0105;KbCtrlF=0x0106;KbCtrlG=0x0107;KbCtrlH=0x0108;
+    KbCtrlI=0x0109;KbCtrlJ=0x010a;KbCtrlK=0x010b;KbCtrlL=0x010c;
+    KbCtrlM=0x010d;KbCtrlN=0x010e;KbCtrlO=0x010f;KbCtrlP=0x0110;
+    KbCtrlQ=0x0111;KbCtrlR=0x0112;KbCtrlS=0x0113;KbCtrlT=0x0114;
+    KbCtrlU=0x0115;KbCtrlV=0x0116;KbCtrlW=0x0117;KbCtrlX=0x0118;
+    KbCtrlY=0x0119;KbCtrlZ=0x011a;
+
+    #// Extended key codes
+
+    KbEsc       = 0x001f;   KbAltSpace  = 0x0234;   KbCtrlIns   = 0x014d;
+    KbShiftIns  = 0x00cd;   KbCtrlDel   = 0x014e;   KbShiftDel  = 0x00ce;
+    KbCtrlShiftIns = 0x01cd; KbCtrlShiftDel = 0x01ce;
+    KbBack      = 0x002a;   KbCtrlBack  = 0x012a;   KbShiftTab  = 0x00ab;
+    KbTab       = 0x002b;   KbAltA=0x0201;          KbAltB=0x0202;
+    KbAltC=0x0203;          KbAltD=0x0204;          KbAltE=0x0205;
+    KbAltF=0x0206;          KbAltG=0x0207;          KbAltH=0x0208;
+    KbAltI=0x0209;          KbAltJ=0x020a;          KbAltK=0x020b;
+    KbAltL=0x020c;          KbAltM=0x020d;          KbAltN=0x020e;
+    KbAltO=0x020f;          KbAltP=0x0210;          KbAltQ=0x0211;
+    KbAltR=0x0212;          KbAltS=0x0213;          KbAltT=0x0214;
+    KbAltU=0x0215;          KbAltV=0x0216;          KbAltW=0x0217;
+    KbAltX=0x0218;          KbAltY=0x0219;          KbAltZ=0x021a;
+    KbCtrlEnter=0x012c;     KbEnter=0x002c;         KbF1=0x0039;
+    KbF2=0x003a;            KbF3=0x003b;            KbF4=0x003c;
+    KbF5=0x003d;            KbF6=0x003e;            KbF7=0x003f;
+    KbF8=0x0040;            KbF9=0x0041;            KbF10=0x0042;
+    KbF11=0x0043;           KbF12=0x0044;
+    KbHome=0x0045;          KbUp=0x0046;            KbPgUp=0x0047;
+    KbLeft=0x0048;          KbRight=0x0049;         KbEnd=0x004a;
+    KbDown=0x004b;          KbPgDn=0x004c;          KbIns=0x004d;
+    KbDel=0x004e;
+    #// These are normal + & - because I think isn't good make any
+    #// difference
+    KbGrayMinus = 0x0035;   KbGrayPlus  = 0x0036;
+    KbShiftF1=0x00b9;       KbShiftF2=0x00ba;       KbShiftF3=0x00bb;
+    KbShiftF4=0x00bc;       KbShiftF5=0x00bd;       KbShiftF6=0x00be;
+    KbShiftF7=0x00bf;       KbShiftF8=0x00c0;       KbShiftF9=0x00c1;
+    KbShiftF10=0x00c2;      KbShiftF11=0x00c3;      KbShiftF12=0x00c4;
+    KbCtrlF1=0x0139;        KbCtrlF2=0x013a;        KbCtrlF3=0x013b;
+    KbCtrlF4=0x013c;        KbCtrlF5=0x013d;        KbCtrlF6=0x013e;
+    KbCtrlF7=0x013f;        KbCtrlF8=0x0140;        KbCtrlF9=0x0141;
+    KbCtrlF10=0x0142;       KbCtrlF11=0x0143;       KbCtrlF12=0x0144;
+    KbAltF1=0x0239;         KbAltF2=0x023a;         KbAltF3=0x023b;
+    KbAltF4=0x023c;         KbAltF5=0x023d;         KbAltF6=0x023e;
+    KbAltF7=0x023f;         KbAltF8=0x0240;         KbAltF9=0x0241;
+    KbAltF10=0x0242;        KbAltF11=0x0243;        KbAltF12=0x0244;
+    KbCtrlPrtSc = 0x0137;   KbCtrlLeft=0x0148;      KbCtrlRight=0x0149;
+    KbCtrlEnd=0x014a;       KbCtrlPgDn=0x014c;      KbCtrlHome  = 0x0145;
+    KbAlt1=0x0221;          KbAlt2=0x0222;          KbAlt3=0x0223;
+    KbAlt4=0x0224;          KbAlt5=0x0225;          KbAlt6=0x0226;
+    KbAlt7=0x0227;          KbAlt8=0x0228;          KbAlt9=0x0229;
+    KbAlt0      = 0x0220;   KbAltMinus  = 0x0235;   KbAltEqual  = 0x0238;
+    KbCtrlPgUp  = 0x0147;   KbNoKey     = 0x0000;
+    KbAltBack=0x022a;
+  
+
+  #//  Keyboard state and shift masks
+  #const ushort
+    KbRightShift  = 0x0001;
+    KbLeftShift   = 0x0002;
+    KbShift       = KbLeftShift | KbRightShift;
+    KbLeftCtrl    = 0x0004;
+    KbRightCtrl   = 0x0004;
+    KbCtrlShift   = KbLeftCtrl | KbRightCtrl;
+    KbLeftAlt     = 0x0008;
+    KbRightAlt    = 0x0008;
+    KbAltShift    = KbLeftAlt | KbRightAlt;
+    KbScrollState = 0x0010;
+    KbNumState    = 0x0020;
+    KbCapsState   = 0x0040;
+    KbInsState    = 0x0080;
+
+
   class Point
     def inspect
       super.sub(">"," <C++::TPoint x=#{x} y=#{y}> >")
@@ -236,55 +316,8 @@ module TVision
   end
 
   class Rect
-    attr_accessor :a, :b
-    def initialize(*args)
-      case args.size
-      when 0
-	@a = Point.new
-	@b = Point.new
-      when 2
-	@a, @b = *args
-      when 4
-	@a = Point.new(*(args[0,2]));
-	@b = Point.new(*(args[2,2]));
-      else
-	raise "needs 0, 2 or 4 arguments"
-      end
-    end
-
-    def move!(aDx, aDy)
-      a.x += aDX
-      a.y += aDY
-      b.x += aDX
-      b.y += aDY
-    end
-    def grow!(aDX, aDY)
-      a.x -= aDX
-      a.y -= aDY
-      b.x += aDX
-      b.y += aDY
-    end
-    def intersect!(other)
-      a.x = [a.x, other.a.x].max
-      a.y = [a.y, other.a.y].max
-      b.x = [b.x, other.b.x].min
-      b.y = [b.y, other.b.y].min
-    end
-    def union!(other)
-      a.x = [a.x, other.a.x].min
-      a.y = [a.y, other.a.y].min
-      b.x = [b.x, other.b.x].max
-      b.y = [b.y, other.b.y].max
-    end		
-    def contains(point)
-      (point.x >= a.x) && (point.x < b.x) &&
-	(point.y >= a.y) && (point.y < b.y)
-    end
-    def ==(other)
-      (a == other.a) && (b == other.b)
-    end
-    def empty?
-      (a.x >= b.x) || (a.y >= b.y)
+    def inspect
+      super.sub(">"," <C++::TRect a=#{a.inspect} b=#{b.inspect}> >")
     end
   end
 
